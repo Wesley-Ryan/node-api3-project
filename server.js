8aa7659f62c9f2ca24247c9413efe40cecdc68b8
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-12-02 12:36:07 -0800

    Added logger

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 
 const server = express();
 
-server.get("/", (req, res) => {
+server.get("/", logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-  console.log(req);
+  console.log("Logger:", req.method, req.url, Date());
   next();
 }
 
